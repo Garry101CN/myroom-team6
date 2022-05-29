a430bcc8452a3369858e6d32c53becfa46fb1b54
data_analyst1214 <26770584+xm1152@users.noreply.github.com>
2022-05-29 22:18:00 +0800

    Add files via upload

--- src/pages/agent_info/index.jsx
+++ src/pages/agent_info/index.jsx
@@ -1,6 +1,71 @@
+import { Card, Layout, Menu } from 'antd';
+import { Form, Input, Button } from 'antd';
+import {React} from 'react';
+import './index.scss'
+
+
+const { Header } = Layout;
+const items1 = ['项目列表', '个人信息'].map(key => ({
+  key,
+  label: `${key}`,
+}));
+
+const onFinish = () => {
+
+}
+const onFinishFailed = () => {
+
+}
+
+const onSubmit = () => {
+
+}
 
 function Info() {
-  return <div>这是经纪人信息页面</div>;
+  // const [formLayout, setFormLayout] = useState('horizontal');
+  return(
+    <Layout className='info-layout'>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+      </Header>
+      <Card className='user-info' title="个人信息">
+        <Form
+          name="basic"
+          layout="horizontal"
+          labelCol={{ span: 3 }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          className="info-form"
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            className='form-item'
+            rules={[{ required: true, message: '请输入用户名!' }]}
+          >
+            <Input maxLength={30} showCount />
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name ="password"
+            className='form-item'
+            rules={[{ required: true, message: '请输入密码!' }]}
+          >
+            <Input.Password visibilityToggle="true" />
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{ offset: 1}}
+          >
+            <Button type="primary" htmlType="submit" onSubmit={onSubmit}>
+              确认
+            </Button>
+          </Form.Item>
+        </Form>  
+      </Card>
+  </Layout>
+  );
 }
 
 export default Info;
